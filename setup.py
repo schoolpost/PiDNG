3c8bc781ce7ef25f5ee7e831628569a1542842bf
Csaba Nagy <schoolpost2009@hotmail.com>
2019-04-08 01:36:17 -0600

    Build error Fix

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('bitunpack', sources = ["bitunpack.c","liblj92/lj92.c"],  extra_compile_args=['-msse2','-std=gnu99'], extra_link_args=[])
+module1 = Extension('bitunpack', sources = ["bitunpack.c","amaze_demosaic_RT.c", "liblj92/lj92.c"],  extra_compile_args=['-msse2','-std=gnu99'], extra_link_args=[])
 
 
 setup ( name = "bitunpack", version = "2.0", description = "Fast bit unpacking functions", ext_modules = [module1])
+
 
