15264b14d5be6429d72d11b6aa12eb13b21cec26
Csaba Nagy <csaba@ualberta.ca>
2020-07-24 14:40:40 -0600

    version update

--- src/setup.py
+++ src/setup.py
@@ -11,7 +11,7 @@ ljpeg92 = Extension('ljpegCompress', sources=[
 
 setup(
     name="pydng",
-    version="3.4.3",
+    version="3.4.4",
     author="Csaba Nagy",
     description="Python utility for converting Raspberry Pi Camera RAW images into Adobe DNG Format.",
     url="https://github.com/schoolpost/PyDNG",
