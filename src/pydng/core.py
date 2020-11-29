50d5f97c8b62c97ed95bd54d181bc55b3a4fb5ca
TRex22 <contact@jasonchalom.com>
2020-11-29 15:56:30 +0200

    Fix or to and :seenoevil:

--- src/pydng/core.py
+++ src/pydng/core.py
@@ -422,7 +422,7 @@ class RPICAM2DNG:
         else:
             profile_embed_array = [profile_embed]
 
-        if (dbr != None or dbr != 'None' or dbr != 'Auto'):
+        if (dbr != None and dbr != 'None' and dbr != 'Auto'):
             dbr_array = dbr
         else:
             dbr_array = [0]
